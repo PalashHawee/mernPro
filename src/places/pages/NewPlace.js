9ee3250d20cbb6288bfed9c67092cc8325282011
Palash Hawee <haweepalash@gmail.com>
2023-02-25 17:59:44 +0530

    Updated Custom Hooks

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,14 +1,21 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import Input from "../../shared/components/FormElements/Input";
 import "./PlaceForm.css";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 import Button from "../../shared/components/FormElements/Button";
+import { useHistory } from "react-router-dom";
 import {
   VALIDATOR_MINLENGTH,
   VALIDATOR_REQUIRE,
 } from "../../shared/util/validators";
 import { useForm } from "../../shared/hooks/form-hook";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+import { AuthContext } from "../../shared/context/auth-context";
 const NewPlace = () => {
+  const auth = useContext(AuthContext);
+  const [isLoading, error, sendRequest, clearError] = useHttpClient();
+
   const [formState, inputHandler] = useForm(
     {
       title: { value: "", isValid: false },
@@ -17,43 +24,61 @@ const NewPlace = () => {
     },
     false
   );
+  const history = useHistory();
 
-  const placeSubmitHandler = (event) => {
+  const placeSubmitHandler = async (event) => {
     event.preventDefault();
-    console.log(formState.inputs); //to know the data stored
+    try {
+      await sendRequest(
+        "http://localhost:5000/api/places",
+        "POST",
+        JSON.stringify({
+          title: formState.inputs.title.value,
+          description: formState.inputs.description.value,
+          address: formState.inputs.title.value,
+          creator: auth.userId,
+        }),
+        { "Content-Type": "application/json" }
+      );
+      history.push("/");
+    } catch (err) {}
   };
 
   return (
-    <form className="place-form" onSubmit={placeSubmitHandler}>
-      <Input
-        id="title"
-        element="input"
-        type="text"
-        label="Title"
-        validators={[VALIDATOR_REQUIRE()]}
-        errorText="Please enter a valid title"
-        onInput={inputHandler}
-      />
-      <Input
-        id="description"
-        element="textarea"
-        label="Description"
-        validators={[VALIDATOR_MINLENGTH(5)]}
-        errorText="Please enter a valid description (at least 5 characters)."
-        onInput={inputHandler}
-      />
-      <Input
-        id="address"
-        element="input"
-        label="Address"
-        validators={[VALIDATOR_REQUIRE()]}
-        errorText="Please enter a valid address."
-        onInput={inputHandler}
-      />
-      <Button type="submit" disabled={!formState.isValid}>
-        ADD PLACE
-      </Button>
-    </form>
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      <form className="place-form" onSubmit={placeSubmitHandler}>
+        {isLoading && <LoadingSpinner asOverLay />}
+        <Input
+          id="title"
+          element="input"
+          type="text"
+          label="Title"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please enter a valid title"
+          onInput={inputHandler}
+        />
+        <Input
+          id="description"
+          element="textarea"
+          label="Description"
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          errorText="Please enter a valid description (at least 5 characters)."
+          onInput={inputHandler}
+        />
+        <Input
+          id="address"
+          element="input"
+          label="Address"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please enter a valid address."
+          onInput={inputHandler}
+        />
+        <Button type="submit" disabled={!formState.isValid}>
+          ADD PLACE
+        </Button>
+      </form>
+    </React.Fragment>
   );
 };
 
--- src/shared/hooks/http-hook.js
+++ src/shared/hooks/http-hook.js
@@ -1,22 +1,51 @@
-import { useState } from "react";
+import { useState, useCallback, useRef, useEffect } from "react";
 
-const useHttpClient = () => {
+export const useHttpClient = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
-  const sendRequest = async (
-    url,
-    method = "GET",
-    body = null,
-    headers = {}
-  ) => {
-    try {
-      const response = await fetch(url, { method, body, headers });
-      const responseData = await response.json();
+  const activeHttpRequests = useRef([]);
 
-      if (!response.ok) {
-        throw new Error(responseData.message);
+  const sendRequest = useCallback(
+    async (url, method = "GET", body = null, headers = {}) => {
+      setIsLoading(true);
+
+      //support an API called AbortController , which is typically used to cancel ongoing fetch requests.
+
+      const httpAbortCtrl = new AbortController();
+      activeHttpRequests.current.push(httpAbortCtrl);
+      try {
+        const response = await fetch(url, {
+          method,
+          body,
+          headers,
+          signal: httpAbortCtrl.signal,
+        });
+        const responseData = await response.json();
+        activeHttpRequests.current = activeHttpRequests.current.filter(
+          (i) => i !== httpAbortCtrl
+        );
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+        setIsLoading(false);
+        return responseData;
+      } catch (err) {
+        setError(err.message);
+        setIsLoading(false);
+        throw err;
       }
-    } catch (err) {}
+    },
+    []
+  );
+  const clearError = () => {
+    setError(null);
   };
+  useEffect(() => {
+    return () => {
+      activeHttpRequests.current.forEach((abortCtrl) => abortCtrl.abort());
+    };
+  }, []);
+  return { isLoading, error, sendRequest, clearError };
 };
